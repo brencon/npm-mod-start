b17680a5bdd823af712f711e733cdf2320674813
Brendan Conrad <brconrad@symitar.com>
2018-11-18 06:51:42 -0800

    moved source to src folder

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-'use strict';
-
-/**
- * Returns the message "This is the npm module starter kit"
- * @return {string}
- */
-module.exports = function() {
-  return 'This is the npm module starter kit';
-};
+module.exports = require('./src');
---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Returns the message "This is the npm module starter kit"
+ * @return {string}
+ */
+module.exports = function() {
+  return 'This is the npm module starter kit';
+};
