f8a1e123eb88895878201ec8c6ec8dffc8e86138
Brendan Conrad <brconrad@symitar.com>
2018-11-18 06:43:06 -0800

    completed working sample unit test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var expect = require('chai').expect;
-var npm-mod-start = require('../index');
+var npmms = require('../index');
 
 describe('npm-mod-start', function() {
-  it('return the string "This is the npm module starter kit"', function() {
-    var result = npm-mod-start;
+  it('return the string "This is the npm module starter kit"', function () {
+    var result = npmms();
     expect(result).to.equal('This is the npm module starter kit');
   });
+});
